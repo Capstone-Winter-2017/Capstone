499955c3becf4dd1a475b3fb45ff03e6cd966eaf
Donnie <w0283980@nscc.ca>
2017-01-25 18:05:38 -0400

    Did some modifications to the welcome screen. changed the route for the members area(will explain). we can get rid of that later if need be. added pictures for the top menu and som css.

--- DnD_app/resources/views/home.blade.php
+++ DnD_app/resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    This Is the Dungeons and Dragons Character Manager
+                    You are logged in!
                 </div>
             </div>
         </div>
