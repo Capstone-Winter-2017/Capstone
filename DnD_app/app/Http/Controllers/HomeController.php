ee1bd1a98d8591d7c499f1090ba6b7c7f67cb13d
w0218409 <w0218409@nscc.ca>
2017-04-13 11:12:32 -0300

    Populating routes via HomeController

--- DnD_app/app/Http/Controllers/HomeController.php
+++ DnD_app/app/Http/Controllers/HomeController.php
@@ -26,4 +26,49 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function myaccount()
+    {
+        return view('myaccount');
+    }
+
+    public function mycharacters()
+    {
+        return view('mycharacters');
+    }
+
+    public function mycampaigns()
+    {
+        return view('mycampaigns');
+    }
+
+    public function mysessions()
+    {
+        return view('mysessions');
+    }
+
+    public function mygroups()
+    {
+        return view('mygroups');
+    }
+
+    public function myfriends()
+    {
+        return view('myfriends');
+    }
+
+    public function mymembers()
+    {
+        return view('mymembers');
+    }
+
+//    public function my()
+//    {
+//        return view('my');
+//    }
+//
+//    public function my()
+//    {
+//        return view('my');
+//    }
 }
